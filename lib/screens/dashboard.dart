701d62b2fd072739c39b357c021935084a42034d
Cezar Mocanu <cmocanu@cisco.com>
2021-08-26 17:12:46 +0300

    [YfBilEjn] Created basic dashboard design

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -7,24 +7,67 @@ import 'package:provider/provider.dart';
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: _AppBarTitle(),
-      ),
-      body: Center(
-        child: _CategoriesListView(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushReplacementNamed(context, '/addGoal');
-        },
-        tooltip: 'Add new goal',
-        child: Icon(Icons.add),
+    return Consumer<AppModel>(
+      builder: (context, app, child) => Scaffold(
+        backgroundColor: Color.fromARGB(255, 250, 250, 250),
+        body: Card(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 12, top: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "${app.income} \$",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Monthly Income",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 12),
+                      child: ElevatedButton.icon(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(context, '/addGoal');
+                        },
+                        label: const Text('Add new goal'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.blue[200],
+                          onPrimary: Colors.blue[900],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: _CategoriesListView(),
+              ),
+            ],
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+        ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+        //     Navigator.pushReplacementNamed(context, '/addGoal');
+        //   },
+        //   tooltip: 'Add new goal',
+        //   child: Icon(Icons.add),
+        // ),
       ),
     );
   }
 }
 
+//TODO add appbar in a better design
 class _AppBarTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -64,14 +107,14 @@ class _CategoryExpandableTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.amber,
+      color: Colors.white,
       child: ExpansionTile(
         title: Text(_category.name),
+        leading: Icon(_category.icon, color: Colors.pink),
         children: _goals
             .map(
-              (goal) => Card(
-                color: Colors.blue,
-                child: ListTile(title: Text("${goal.title}")),
+              (goal) => ListTile(
+                title: Text("${goal.title}"),
               ),
             )
             .toList(),
