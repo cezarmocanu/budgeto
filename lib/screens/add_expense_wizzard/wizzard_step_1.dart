98953910dbba36c733cb00173a2aa3eebe8eb240
Cezar Mocanu <cezarmocanu@Cezars-MacBook-Air.local>
2021-09-19 10:50:06 +0300

    [ncDQesem] Added wizzard screen

--- lib/screens/add_expense_wizzard/wizzard_step_1.dart
+++ lib/screens/add_expense_wizzard/wizzard_step_1.dart
@@ -1,3 +1,5 @@
+import 'package:budgeto_flutter/common/form/styled-form-header.dart';
+import 'package:budgeto_flutter/constants/routes.dart';
 import 'package:flutter/material.dart';
 
 class WizzardStep1 extends StatelessWidget {
@@ -5,18 +7,62 @@ class WizzardStep1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Card(
-              child: Text("Unique expanese"),
+        child: Card(
+          margin: EdgeInsets.all(20),
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                StyledFormHeader(
+                  title: 'Add a new expense',
+                  backRoute: RoutesEnum.Dashboard,
+                ),
+                WizzardCardButton(
+                  label: 'Unique Payment',
+                  onTap: () => goTo(context, RoutesEnum.AddExpense),
+                ),
+                WizzardCardButton(
+                  label: 'Recursive Payment',
+                  onTap: () => {},
+                ),
+                WizzardCardButton(
+                  label: 'Fund Goal',
+                  onTap: () => {},
+                ),
+              ],
             ),
-            Card(
-              child: Text("Unique expanese"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WizzardCardButton extends StatelessWidget {
+  final String label;
+  final Function() onTap;
+
+  WizzardCardButton({required this.label, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Card(
+          elevation: 3,
+          margin: EdgeInsets.only(top: 10),
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(label),
+                Icon(Icons.arrow_right),
+              ],
             ),
-            Card(
-              child: Text("Unique expanese"),
-            )
-          ],
+          ),
         ),
       ),
     );
